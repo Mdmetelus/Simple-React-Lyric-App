aedeee8879c2b2ca4e9549cfc7d6316ecccea6e5
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-11 00:54:25 -0500

    "consumer added to Tracks page"

--- simple_lyric/src/Components/Tracks/Tracks.js
+++ simple_lyric/src/Components/Tracks/Tracks.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Consumer } from '../../Context';
 
 export default class Tracks extends Component {
   render() {
     return (
       <div>
           <h1>Tracks</h1>
+          <Consumer>
+              {value => {console.log(value);
+            <h1>Tracks</h1>}}
+          </Consumer>
         
       </div>
     )

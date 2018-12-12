f5ed1ee394c28a300ad1c249bd8ed88881d930d2
Mr. Metelus <Mdmetelus@gmail.com>
2018-12-12 14:34:33 -0500

    "imports included, if statment with spinnner and map function."

--- simple_lyric/src/Components/Tracks/Tracks.js
+++ simple_lyric/src/Components/Tracks/Tracks.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Consumer } from '../../Context';
+import Spinner from '../Layout/Spinner';
+import Track from '../Tracks/Track';
 
 class Tracks extends Component {
   render() {
@@ -7,8 +9,24 @@ class Tracks extends Component {
         <Consumer>
               {value => {
                 console.log(value);
-                return( <h1>Tracks</h1>
-                  );
+                if(value.track_list === undefined || value.track_list.length === 0) {
+                    return <Spinner />
+
+                } else {
+                    return(
+                        <React.Fragment>
+                            <h3 className="text-Center">{value.heading}
+                            
+                            </h3>
+
+                        <div className="row" >
+                            {.value.track_list.map(item =>(
+                                <Track key={item.track.track_id} track={item.track}/>
+                            ))}
+                        </div>
+                        </React.Fragment>
+                    );<h1>Tracks Loaded</h1>
+                }
               }
                   }
           </Consumer>
